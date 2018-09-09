05ef769c77ab64d1b43c586e502cf7de40063e04
Rockheung <rockheung@gmail.com>
2018-09-10 07:06:16 +0900

    update

---
+++ python/2504.py
@@ -0,0 +1,23 @@
+
+def cal_unit(ps,
+             buf=0):
+    try:
+        s = ps.pop()
+        while(ps[-1] == ')' or ps[-1] == ']'):
+            ps, buf_tmp = cal_unit(ps)
+            buf += buf_tmp
+        buf = 1 if buf == 0 else buf
+        if ps[-1] == '(' and s == ')':
+            ps.pop()
+            return ps, buf*2
+        elif ps[-1] == '[' and s == ']':
+            ps.pop()
+            return ps, buf*3
+        raise IndexError
+    except IndexError:
+        return ps,0
+
+
+if __name__ == '__main__':
+    ps, val = cal_unit(list(input().join(['[',']'])))
+    print(val//3 if len(ps) == 0 else 0)
