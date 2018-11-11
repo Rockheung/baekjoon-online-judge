216ab10a8674cca842befdc9eb189f9113adacb6
Rockheung <rockheung@gmail.com>
2018-11-11 14:35:02 +0900

    update

--- python/1561/1561.py
+++ python/1561/1561.py
@@ -1,3 +1,25 @@
+def time(n, m, play_time):
+    t = 0
+    while(True):
+        t += 1
+        children = sum([t%x for x in play_time])
+        yield children
+
+def _1561(n, m, play_time):
+    play = [0 for _ in range(m)]
+    i = idx = t = 0
+    children = time(n,m,play_time)
+    child = next(children)
+    for _ in range(n):
+        i += 1
+        idx = play.index(min(play))
+        play[idx] += play_time[idx]
+        print(i,'child on play: ', play)
+        if i == child:
+            t += 1
+            child = next(children)
+            print('============== {} mins ============= {} children\n'.format(t, child))
+    return idx+1
 
 def time_child(n,m,m_t):
     time = child = 0
@@ -10,7 +32,7 @@ def time_child(n,m,m_t):
 
 
 # N: Children, M: Number of cars, M_T: time per car
-def _1561(n, m, m_t):
+def _1561_2(n, m, m_t):
     buf = [ 0 for _ in range(m) ]
     child = last_idx = 0
     children = time_child(n,m,m_t)
