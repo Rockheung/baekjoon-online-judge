8a2086ca82217e6cf67633c8b34239a3b00ac2d1
Rockheung <rockheung@gmail.com>
2018-11-11 22:39:59 +0900

    update

--- python/1561/1561.py
+++ python/1561/1561.py
@@ -1,59 +1,55 @@
-def time(n, m, play_time):
-    t = 0
-    while(True):
+#
+# https://www.acmicpc.net/problem/1561
+
+# Generator for getting number of children as many as possible
+# at each time
+def time_(n, m, times):
+    t = child = 0
+    while (n >= child):
         t += 1
-        children = sum([t%x for x in play_time])
-        yield children
-
-def _1561(n, m, play_time):
-    play = [0 for _ in range(m)]
-    i = idx = t = 0
-    children = time(n,m,play_time)
-    child = next(children)
-    for _ in range(n):
-        i += 1
-        idx = play.index(min(play))
-        play[idx] += play_time[idx]
-        print(i,'child on play: ', play)
-        if i == child:
-            t += 1
-            child = next(children)
-            print('============== {} mins ============= {} children\n'.format(t, child))
-    return idx+1
-
-def time_child(n,m,m_t):
-    time = child = 0
-    while(n >= child):
-        time += 1
-        each_play = [time//mt for mt in m_t]
-        left_time = [time%mt for mt in m_t]
+        each_play = [t//mt for mt in times]
+        left_time = [t%mt for mt in times]
         child = sum(each_play)
-        yield time, child, each_play, left_time
+        yield t, child, each_play, left_time
 
 
-# N: Children, M: Number of cars, M_T: time per car
-def _1561_2(n, m, m_t):
+def sol0(n, m, times):
+    cars = [0 for _ in range(m)]
+    idx, t = 0, 1
+    for i in range(n):
+        cars[idx] += times[idx]
+        pre_idx, idx = idx, cars.index(min(cars))
+        print(t, pre_idx, idx, cars)
+        if pre_idx >= idx : t += 1
+    return pre_idx+1, cars[pre_idx]
+
+
+# N: Children, M: Number of cars, times: time per car
+def sol1(n, m, times):
     buf = [ 0 for _ in range(m) ]
     child = last_idx = 0
-    children = time_child(n,m,m_t)
-    time, child = next(children)[:2]
+    children = time_(n,m,times)
+    t, child = next(children)[:2]
     for i in range(n):
         # last_idx = buf.index(min(buf))
-        # buf[last_idx] += m_t[last_idx]
-        buf = [time - time%x for j, x in enumerate(m_t)]
+        # buf[last_idx] += times[last_idx]
+        buf = [t - t%x for j, x in enumerate(times)]
         if (i+1 < child):
-            time, child = next(children)
-        print('{:2d} @ {:2d} mins, {}'.format(i+1,time,buf))
+            t, child = next(children)
+        print('{:2d} @ {:2d} mins, {}'.format(i+1,t,buf))
     return last_idx + 1
 
 
-def _1561_binary_search(n, m, m_t):
-    lower, upper = 0, n * max(m_t)
+# Problem: If how_many_children == condition provided when running,
+# this way works well, but else we should calculate every each step.
+# Good example: 22 children, 5 plays, 1,2,3,4,5 mins on each play.
+def sol2(n, m, times):
+    lower, upper = 0, n * max(times)
     while (upper > lower):
         # even number => clearly half, odd number => bigger integer
         ref_time = (upper + lower)//2 if (upper + lower)%2 == 0 else (upper + lower)//2 + 1
         if ref_time == upper: break
-        how_many_children = sum([ref_time//x for x in m_t])
+        how_many_children = sum((ref_time//x for x in times))
         if how_many_children > n:
             upper = ref_time
         elif how_many_children < n :
@@ -61,38 +57,44 @@ def _1561_binary_search(n, m, m_t):
         else :
             break
     # min_ref_time <= ref_time, same or less
-    min_ref_time = ref_time - min([ref_time%x for x in m_t])
+    min_ref_time = ref_time - min((ref_time%x for x in times))
     # can not be 0
-    gap_children = n - sum([(min_ref_time-1)//x for x in m_t])
-    pre_step_idx = [i+1 for i, x in enumerate([(min_ref_time-1)%x for x in m_t]) if m_t[i] - 1 == x]
+    gap_children = n - sum(((min_ref_time-1)//x for x in times))
+    pre_step_idx = [i+1 for i, x in enumerate([(min_ref_time-1)%x for x in times]) if times[i] - 1 == x]
     print(ref_time, min_ref_time, gap_children, pre_step_idx)
     return pre_step_idx[gap_children-1] # if gap_children > 0 else pre_step_idx[0]
 
 
-def test(n, m, m_t):
+def sol3(n, m, times):
+    lower, upper = 0, n * max(times)
+    while (upper > lower):
+        ref_time = (upper + lower) //2
+        if ref_time == lower: break
+        maximum_children = sum((ref_time//x for x in times))
+        if maximum_children > n:
+            upper = ref_time
+        elif maximum_children < n:
+            lower = ref_time + 1
+        else:
+            exact_n = True
+
+
+
+
+
+
+def test(n, m, times):
     t = nc = 0
     while (n >= nc):
-        nc = sum([t//x for x in m_t])
-        print(t,nc, [x*(t//x) for x in m_t])
+        nc = sum([t//x for x in times])
+        print(t,nc, [x*(t//x) for x in times])
         t += 1
 
 
 
 if __name__ == '__main__':
-    # for _ in range(int(input())):
-    #     n,m = map(int, tuple(input().split()))
-    #     play_time = [int(x) for x in list(input().split())]
-    #     print(ans(n,m,play_time))
-
-
     n,m = map(int, tuple(input().split()))
-    play_time = [int(x) for x in input().split()]
-    # test(n,m,play_time)
-    # print(ans_1561_reversed(n,m,play_time))
-    for t,c,ec,lt in time_child(n,m,play_time):
+    times = [int(x) for x in input().split()]
+    for t,c,ec,lt in time_(n,m,times):
         print('time: ', t, '\tchild: ', c, '\teach play: ', ec, '\tleft time: ', lt)
-    # print('the answer: ',_1561(n,m,play_time))
-
-    # n,m = map(int, tuple(input().split()))
-    # play_time = [int(x) for x in input().split()]
-    print(_1561_binary_search(n,m,play_time))
+    print('Last played car index: {0}, takes time: {1}'.format(*sol0(n,m,times)))
