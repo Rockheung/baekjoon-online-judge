7836600a3e6bc9a7ed5cbecc8a2c19074e1eaa34
Rockheung <rockheung@gmail.com>
2018-11-13 22:46:34 +0900

    update

--- python/1561/1561.py
+++ python/1561/1561.py
@@ -55,7 +55,7 @@ def sol1(n, m, times):
 # this way works well, but else we should calculate every each step.
 # Good example: 22 children, 5 plays, 1,2,3,4,5 mins on each play.
 def sol2(n, m, times):
-    lower, upper = n * min(times), n * max(times)
+    lower, upper = 0, n * max(times)
     while (upper > lower):
         # even number => clearly half, odd number => bigger integer
         ref_time = (upper + lower)//2 if (upper + lower)%2 == 0 else (upper + lower)//2 + 1
@@ -74,6 +74,28 @@ def sol2(n, m, times):
     pre_step_idx = [i+1 for i, x in enumerate([(min_ref_time-1)%x for x in times]) if times[i] - 1 == x]
     print(ref_time, min_ref_time, gap_children, pre_step_idx)
     return pre_step_idx[gap_children-1] # if gap_children > 0 else pre_step_idx[0]
+
+
+def damn_right(n,m,ts):
+    k = n//m if n%m else n//m - 1
+    # 모든 놀이기구의 탑승 시간이 min(tx) 인 경우보다는 무조건 길거나 같고,
+    # max(tx) 인 경우보다는 무조건 짧거나 같다.
+    lo, hi = min(ts)*k, max(ts)*k
+    md = (lo+hi)//2
+    while lo < hi:
+        # 루프문을 빠지는 경우 무조건 True인 경우가 실행되고 나서다.
+        print(lo, hi, md, sum((md+t)//t for t in ts))
+        if sum((md+t)//t for t in ts) < n:
+            lo = md+1
+            md = (lo+hi)//2
+        else:
+            hi = md
+            md = lo if lo+1==hi else (lo+hi)//2
+    print(lo, hi, md, sum((md+t)//t for t in ts))
+    l = [i for (i, t) in enumerate(ts, 1) if not md%t]
+    idx = n-1-sum((md+t)//t for t in ts)
+    print(l,idx)
+    return l[idx]
 
 
 def sol3(n, m, times):
@@ -111,9 +133,9 @@ def test(n, m, times):
         t += 1
 
 
-
 if __name__ == '__main__':
     nm = [int(i) for i in input().split()]
     times = [int(t) for t in input().split()]
-    print('Last played car index: {0}, takes maximum time: {2}, \n{3}'.format(*sol0(*nm,times)))
-    print(sol3(*nm, times))
+    #print('Last played car index: {0}, takes maximum time: {2}, \n{3}'.format(*sol0(*nm,times)))
+    #print(sol3(*nm, times))
+    print(damn_right(*nm, times))
